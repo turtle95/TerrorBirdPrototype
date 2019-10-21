d72013589167d29ab207981c36bd31af1e8939f4
Joshua Watts <joshuawatts3@gmail.com>
2019-10-20 19:55:47 -0600

    birds can fight now, tree can die

--- Assets/Script/Entity.cs
+++ Assets/Script/Entity.cs
@@ -14,16 +14,20 @@ public class Entity : MonoBehaviour
 
     public string moveTag = "MoveCol";
 
+    public int level = 0;
+    public int attack = 1;
 
+    public int exp = 0;
+    public int expTillLvlUp = 1;
+
+    public string enemyTag = "Enemy";
+    public GameObject explosion;
 
     public void MoveTo(Vector3 targetPos)
     {
         target = targetPos;
         moving = true;
     }
-
-
-
 
     private void Update()
     {
@@ -37,10 +41,13 @@ public class Entity : MonoBehaviour
         {
             rb.velocity = Vector3.zero;
         }
+
+        if(exp > expTillLvlUp)
+        {
+            LevelUp();
+            expTillLvlUp = exp + level;
+        }
     }
-
-
-
 
     private void OnTriggerEnter(Collider col)
     {
@@ -48,5 +55,35 @@ public class Entity : MonoBehaviour
         {
             moving = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag(enemyTag))
+        {
+            col.gameObject.GetComponent<Enemy>().health--;
+            if (col.gameObject.GetComponent<Enemy>().health <= 0)
+                exp++;
+        }
+    }
+
+    public void TakeDamage(int damage = 2)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(this.gameObject);
+        }        
+    }
+
+    private void LevelUp()
+    {
+        level+= 2;
+        health += level;
+        attack += level;
+        this.transform.localScale += Vector3.one;
+        moveSpeed = new Vector3(0, 0, moveSpeed.z + level);
     }
 }
